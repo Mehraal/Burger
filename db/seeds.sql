31eecdf83a965f002647d3f7c06edf6daaa8b2f1
Alex Mehr <alirezaalipouri@gmail.com>
2018-07-29 12:20:10 -0700

    mysql burger created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES("Mushroom CheeseBurger",true);
+INSERT INTO burgers (burger_name) VALUES("Bacon CheeseBurger",true);
+INSERT INTO burgers (burger_name) VALUES("Veggie Burger", false)
+
